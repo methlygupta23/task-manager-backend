a640ffe8c27903c0d9589fc4ef953c15520e0d9a
methlygupta23 <methlygupta7@gmail.com>
2025-11-20 21:11:03 +0530

    deploy

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-app.use(cors());
+// Configure CORS for production and development
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:4000'],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
